c26fa0f1edcef8ff0c948b14a90118c439039737
Vineet <vineetgoyat1@gmail.com>
2026-05-31 19:05:12 +0530

    modified if else

--- ifelse.cpp
+++ ifelse.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int age;
-    cout<<"The age is:"<<endl;
-    cin>>age;
-    if(age>150 || age<1)
+    cout << "The age is:" << endl;
+    cin >> age;
+    if (age > 150 || age < 1)
     {
-        cout<<"Invalid age"<<endl;
+        cout << "Invalid age" << endl;
     }
-    else if(age>=18){
-        cout<<"You can vote"<<endl;
+    else if (age >= 18)
+    {
+        cout << "You can vote" << endl;
     }
-    else{
-        cout<<"You cannot vote"<<endl;
+    else
+    {
+        cout << "You cannot vote" << endl;
     }
-return 0;
+    return 0;
 }
