d56d7df69aebffc01db8a813ea50742c3173068d
Vineet <vineetgoyat1@gmail.com>
2026-05-31 18:50:23 +0530

    operators c++

---
+++ operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p,v;
+    cout<<"The value of p is:"<<endl;
+    cin>>p;
+    cout<<"the value of v is:"<<endl;
+    cin>>v;
+
+    cout<<"Sum is:"<<p+v<<endl;
+    cout<< "Difference is:"<<p-v<<endl;
+    cout<<"Product is:"<<p*v<<endl;
+    cout<<"Quotient is:"<<(float)p/v<<endl;
+}
