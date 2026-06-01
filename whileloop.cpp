da7b97b52a206ee4c57e974f889d43b81aede36a
Vineet <vineetgoyat1@gmail.com>
2026-06-01 20:57:22 +0530

    chidiya udi by while loop

---
+++ whileloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int chidiya=0;
+    while(chidiya<30){
+        cout<<"Chidiya udi"<< chidiya <<endl;
+        chidiya++;
+    }
+
+    return 0;
+}
