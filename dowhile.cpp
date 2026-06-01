61950d3a1f247b991621054d314bbc9dff0b8e57
Vineet <vineetgoyat1@gmail.com>
2026-06-01 21:05:58 +0530

    do while basic

---
+++ dowhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int kutta = 0;
+    do
+    {
+        cout << "Kutta bhonka - " << kutta << endl;
+        kutta++;
+    } while (kutta < 30);
+    return 0;
+}
