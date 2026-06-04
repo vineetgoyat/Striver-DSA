d77c993639a36054dce08e1264c8156f7d14aa9d
Vineet <vineetgoyat1@gmail.com>
2026-06-05 00:04:21 +0530

    finally

--- ssort.cpp
+++ ssort.cpp
@@ -9,12 +9,12 @@ public:
         for(int i=0; i<nums.size(); i++){
             int min=nums[i];
             for(int j=i+1; j<nums.size(); j++){
-                if (nums[j] <= nums[i]) continue;
+                if (nums[i] <= nums[j]) continue;
                 else {
                     int temp;
                     temp=nums[i];
                     nums[i] = nums[j];
-                    nums[j] = nums[i];
+                    nums[j] = temp;
                 }
             }
         }
@@ -27,7 +27,7 @@ public:
 int main() {
 
     Solution obj;
-    vector<int> result = {7, 4, 5};
+    vector<int> result = {5 ,4 ,4 ,1 ,1};
     obj.selectionSort(result);
 
     for (int i = 0; i < result.size(); i++) {
