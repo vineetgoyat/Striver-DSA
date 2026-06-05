ffde63c00efd53df236cdc547bce970e4deea832
Vineet <vineetgoyat1@gmail.com>
2026-06-05 14:11:23 +0530

    truly understood with revision no.1

---
+++ selectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+    public:
+    vector<int>SelectionSort(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            int min=nums[i];
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]<=nums[j]) continue;
+                else{
+                int temp;
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                }
+            
+            }
+        }
+        return nums;
+    }
+};
+
+
+int main(){
+    Solution obj;
+    vector<int> result ={5, 7, 2, 9, 4};
+    obj.SelectionSort(result);
+    
+    for(int i=0; i<result.size(); i++){
+        cout<< result[i] <<" ";
+    }
+}
