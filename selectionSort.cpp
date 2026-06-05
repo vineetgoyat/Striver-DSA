4797fa930429922ab33442b72aada56f29fc5800
Vineet <vineetgoyat1@gmail.com>
2026-06-05 14:35:28 +0530

    a minor understanding update

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -5,7 +5,10 @@ using namespace std;
 class Solution{
     public:
     vector<int>SelectionSort(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size()-1; i++){
+            //i last second tak jayegi
+            //or j last tak jayegi
+            //or min ko bhi i se badal sakte hai 
             int min=nums[i];
             for(int j=i+1; j<nums.size(); j++){
                 if(nums[i]<=nums[j]) continue;
