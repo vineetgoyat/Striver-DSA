d1cd5cc530a07cbe9eb1319c927f009bac0d301d
Vineet <vineetgoyat1@gmail.com>
2026-06-12 19:54:00 +0530

    Marks entering system by using array

---
+++ arrays/arraybasic2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3] = {1, 2, 3};
+    //array index=0, 1, 2
+
+    int marks[6];
+
+    for(int i=1; i<6; i++){
+        cout<<"Marks of the student: "<<i<<endl;
+        cin>>marks[i];
+    }
+
+}
