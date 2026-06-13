2a1091344ca2962f9b28863c82ddc26f01b846c3
Vineet <vineetgoyat1@gmail.com>
2026-06-13 16:50:04 +0530

    typecasting

---
+++ typecasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3353;
+    float b= 3.14;
+    cout<< "the value of a is: "<<(float)a/3<<endl;
+
+    cout<< "the value of b is: "<<(int)b<<endl;
+}
