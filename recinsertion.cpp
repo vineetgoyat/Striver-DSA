eea3fe8697cbd6522895bcb6a50e25208aa15c17
Vineet <vineetgoyat1@gmail.com>
2026-06-23 22:38:30 +0530

    Trying recursive insertion sort

---
+++ recinsertion.cpp
@@ -0,0 +1,38 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void recinsort(vector<int>& arr) {
+        
+        for(int i=1;i<arr.size();i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && key<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        
+        
+    return arr;
+        
+    }
+    
+};
+
+
+int main() {
+
+    Solution obj;
+    vector<int> result = {7 ,9 ,6 ,2 ,4};
+    obj.recinsort(result);
+
+    for (int i = 0; i < result.size(); i++) {
+    cout << result[i] << " ";
+    }
+}   
