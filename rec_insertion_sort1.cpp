04092287da7d2a870e134706b2b367345545e66b
Vineet <vineetgoyat1@gmail.com>
2026-06-24 17:02:30 +0530

    resolved

--- rec_insertion_sort1.cpp
+++ rec_insertion_sort1.cpp
@@ -6,25 +6,27 @@ using namespace std;
 
 class Solution {
 public:
-    void rec_insertion_sort(int arr, int i, int n){
-        if(i==n)return;
-        
-        int j=i;
+    void rec_insertion_sort(vector<int> &arr, int i, int n){
+        if(i==n) return;
+
+        int j = i;
         while(j>0 && arr[j]<arr[j-1]){
-            int temp=arr[j];
-            arr[j]=arr[j-1];
-            arr[j-1]=temp;
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
             j--;
         }
         rec_insertion_sort(arr, i+1, n);
     }
+};
 
 
 int main() {
 
     Solution obj;
-    void result = {7 ,9 ,6 ,2 ,4};
-    obj.rec_insertion_sort(result);
+    vector<int> result = {7, 9, 6, 2, 4};
+    obj.rec_insertion_sort(result, 1, result.size());
 
-    for ( int x : result) { cout << x << " "; }
+    for (int x : result) cout << x << " ";
+    cout << '\n';
 }
